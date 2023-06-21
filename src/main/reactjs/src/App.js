12f5eb1d5d8d1a5a713f5a3e711157d25981d85b
SeokjinKim <jameskim0208@gmail.com>
2023-06-21 16:34:51 +0900

    master First commit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import carimg from './car1.png';
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={carimg} width={300} border="1"/>
+      <br/><br/>
+      <h2>Docker 배포 성공 기원!!!!</h2>
+    </div>
+  );
+}
+
+export default App;
