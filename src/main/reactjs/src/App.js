08b2553e3dce38e51757179a62b9ef9e9e51f99b
SeokjinKim <jameskim0208@gmail.com>
2023-06-22 16:13:34 +0900

    리엑트 변경

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -6,6 +6,7 @@ function App() {
       <img alt='' src={carimg} width={300} border="1"/>
       <br/><br/>
       <h2>Docker 배포 성공 기원!!!!</h2>
+      <h1>우와 실시간으로 바뀌네? 대박!!!</h1>
     </div>
   );
 }
