07a2759ae287b783a9b7f5ecfef315a889abdcf8
SeokjinKim <jameskim0208@gmail.com>
2023-06-23 17:53:50 +0900

    6월23일 최종 커밋

--- src/main/reactjs/src/components/Menu.js
+++ src/main/reactjs/src/components/Menu.js
@@ -3,6 +3,7 @@ import '../App.css';
 import { NavLink } from 'react-router-dom';
 
 function Menu(props) {
+
     return (
         <ul className='menu'>
             <li>
@@ -17,9 +18,24 @@ function Menu(props) {
             <li>
                 <NavLink to={"/board/list"}>게시판</NavLink>
             </li>
+            {
+                sessionStorage.loginok==null || sessionStorage.loginok==='no'?
             <li>
                 <NavLink to={"/login"}>로그인</NavLink>
-            </li>
+            </li>:
+            <div>
+                <li style={{width:'250px',backgroundColor:'darkcyan',color:'white',cursor:'pointer'}}
+                onClick={()=>{
+                    sessionStorage.removeItem("loginok");
+                    sessionStorage.removeItem("myid");
+                    sessionStorage.removeItem("myname");
+                    window.location.reload();//새로고침
+                }}>로그아웃
+                &nbsp;&nbsp;&nbsp;
+                <b style={{color:'yellow'}}>{sessionStorage.myname}({sessionStorage.myid})님</b>
+                </li>
+            </div>
+            }
         </ul>
     );
 }
--- src/main/reactjs/src/member/MemberForm.js
+++ src/main/reactjs/src/member/MemberForm.js
@@ -1,39 +1,178 @@
 import React, { useState } from 'react';
 import '../App.css';
-import DaumPostcodeEmbed from 'react-daum-postcode';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import DaumPostcode, { DaumPostcodeEmbed } from 'react-daum-postcode';
+import Axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+
 
 function MemberForm(props) {
-    const [openPostcode, setOpenPostcode] = useState(false);
+    
+    const[myid,setMyid]=useState('');
+    const[mypass,setMypass]=useState('');
+    const[myaddress1,setMyaddress1]=useState('');
+    const[myaddress2,setMyaddres2]=useState('');
+    const[myname,setMyname]=useState('');
+    const [btnok,setBtnok]=useState(false);
 
-    /**
-     * handler
-     */
+    const [openPostcode, setOpenPostcode] = React.useState(false);
+
+
+
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+  
+    const handleClickOpen = () => {
+      setOpen(true);
+      setOpenPostcode(current => !current);
+
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+      setOpenPostcode(false);
+
+    };
+
     const handle = {
-        // 버튼 클릭 이벤트
-        clickButton: () => {
-            setOpenPostcode(current => !current);
-        },
-
         // 주소 선택 이벤트
         selectAddress: (data) => {
             console.log(`
                 주소: ${data.address},
+                아파트 명: ${data.buildingName}
                 우편번호: ${data.zonecode}
             `)
+            setMyaddress1(`(${data.zonecode})${data.address}${data.buildingName}`);
+            setOpen(false);
             setOpenPostcode(false);
         },
     }
+
+
+    const onSubmitEvent=(e)=>{
+        e.preventDefault();//기본 이벤트 무효화
+        if(!btnok){
+            alert("중복확인 버튼을 눌러주세요!!");
+            return;
+        }
+        Axios.post("/member/insert",{myname,myid,mypass,myaddress:`${myaddress1} ${myaddress2}`})
+        .then(res=>{
+            alert("가입되었습니다.");
+            navi("/login");
+        })
+    }
+    //중복확인 버튼 이벤트
+    const btnJungbok=()=>{
+        let url="/member/searchid?myid="+myid;
+        Axios.get(url)
+        .then(res=>{
+            if(Number(res.data)==0){
+                alert("사용 가능한 아이디입니다.");
+                setBtnok(true);
+
+            }else{
+                alert("이미 가입된 아이디 입니다.");
+                setMyid('');
+                setBtnok(false);
+            }
+        })
+    }
+    const navi=useNavigate();
     return (
         <div>
-            <h1>회원가입 폼</h1>
-            <button onClick={handle.clickButton}>toggle</button>
+            <form onSubmit={onSubmitEvent}>
+                <table className='table' style={{width:'500px'}}>
+                    <caption align="top"><b>회원가입</b></caption>
+                    <tbody>
+                        <tr>
+                        <th style={{width:'100px',background:'#b0e0e6'}}>이름</th>
+                        <td>
+                            <input type='text' className='form-control'
+                            placeholder='이름입력' required 
+                            value={myname} onChange={(e)=>setMyname(e.target.value)}/>
+                        </td>
+                        </tr>
+                        <tr>
+                        <th style={{width:'100px',background:'#b0e0e6'}}>아이디</th>
+                        <td className='input-group'>
+                            <input type='text' className='form-control'
+                            placeholder='아이디' required 
+                            value={myid} onChange={(e)=>{setMyid(e.target.value)
+                            setBtnok(false)}}/>
+                            <button type='submit' className='btn btn-outline-danger btn-sm'
+                            onClick={btnJungbok}>중복확인</button>
+                        </td>
+                        </tr>
+                        <tr>
+                        <th style={{width:'100px',background:'#b0e0e6'}}>비밀번호</th>
+                        <td>
+                            <input type='password' className='form-control'
+                             required style={{width:'120px'}}
+                            value={mypass} onChange={(e)=>setMypass(e.target.value)}/>
+                        </td>
+                        </tr>
+                        <tr>
+                        <th style={{width:'100px',background:'#b0e0e6'}}>주소</th>
+                        <td>
+                            <div className='input-group'>
+                            <input type='text' className='form-control' required readOnly
+                             value={myaddress1}/>
+                             <button type='button' className='btn btn-sm btn-outline-success'
+                             variant="outlined" onClick={handleClickOpen}>주소검색</button>
+                            </div>
+                            <div>
+                                <input type='text' className='form-control'
+                                value={myaddress2} onChange={(e)=>setMyaddres2(e.target.value)}
+                                placeholder='상세주소를 입력해주세요'/>
+                            </div>
+                        </td>
+                        </tr>
 
-            {openPostcode && 
+                        <tr>
+                            <td colSpan={2} align='center'>
+                                <button type='submit' className='btn btn-outline-info'
+                                style={{width:'100px'}}>가입하기</button>
+                            </td>
+                            
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+            {/* 다이얼로그 */}
+      <Dialog
+        fullScreen={fullScreen}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">
+          우편번호 검색
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+          {openPostcode && 
                 <DaumPostcodeEmbed 
                     onComplete={handle.selectAddress}  // 값을 선택할 경우 실행되는 이벤트
                     autoClose={false} // 값을 선택할 경우 사용되는 DOM을 제거하여 자동 닫힘 설정
                     defaultQuery='강남대로 94길 20' // 팝업을 열때 기본적으로 입력되는 검색어 
                     />}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            취소
+          </Button>
+        </DialogActions>
+      </Dialog>
         </div>
     );
 }
